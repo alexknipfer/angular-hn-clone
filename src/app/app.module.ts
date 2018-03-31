b745f14d9c015c6b9498689e3e9d5f45af4e6529
alexknipfer <alexanderknipfer@gmail.com>
2018-03-30 23:54:39 -0500

    Add data service and http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./components/nav/nav.component";
+import { DefaultComponent } from "./layouts/default/default.component";
+import { PostsComponent } from "./views/posts/posts.component";
+
+import { DataService } from "./services/data.service";
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [AppComponent, NavComponent, DefaultComponent, PostsComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
