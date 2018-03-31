77d8813b0aef3bf68be19644f59b505306b114d7
alexknipfer <alexanderknipfer@gmail.com>
2018-03-31 16:15:05 -0500

    Use models for return types

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
+import { User } from '../shared/models/user.model'
+import { Observable } from 'rxjs/Observable'
+import { Post } from '../shared/models/post.model'
 
 @Injectable()
 export class DataService {
@@ -7,14 +10,14 @@ export class DataService {
 
   constructor(public http: HttpClient) {}
 
-  getPosts() {
+  getPosts(): Observable<Post[]> {
     const apiUrl = this._getApiUrl('posts')
-    return this.http.get(apiUrl)
+    return this.http.get<Post[]>(apiUrl)
   }
 
-  getUsers() {
+  getUsers(): Observable<User[]> {
     const apiUrl = this._getApiUrl('users')
-    return this.http.get(apiUrl)
+    return this.http.get<User[]>(apiUrl)
   }
 
   _getApiUrl(urlSegment: string) {
