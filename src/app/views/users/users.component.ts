06ea22a5aae4bffd10434b1fa66a29823bc787a0
alexknipfer <alexanderknipfer@gmail.com>
2018-03-31 16:02:19 -0500

    Fetch and display users in table

--- src/app/views/users/users.component.ts
+++ src/app/views/users/users.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { DataService } from '../../services/data.service'
+
+interface IUser {
+  username: string
+  name: string
+  email: string
+  phone: string
+}
 
 @Component({
   selector: 'app-users',
@@ -6,10 +14,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: any[]
+  loading: boolean = true
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
+    this.dataService.getUsers().subscribe((users: IUser[]) => {
+      this.loading = false
+      this.users = users
+    }, this.handleError)
   }
 
+  handleError(error) {
+    this.loading = false
+    console.log('there was an error: ', error.message)
+  }
 }
