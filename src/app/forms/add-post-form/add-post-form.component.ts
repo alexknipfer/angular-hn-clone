1aa15746707adce60409f6bb78ae6516ac0f40f1
alexknipfer <alexanderknipfer@gmail.com>
2018-04-03 21:50:21 -0500

    Add form group and form control

--- src/app/forms/add-post-form/add-post-form.component.ts
+++ src/app/forms/add-post-form/add-post-form.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-add-post-form',
   templateUrl: './add-post-form.component.html',
   styleUrls: ['./add-post-form.component.css']
 })
-export class AddPostFormComponent {
-  submitted: boolean = false
+export class AddPostFormComponent implements OnInit {
+  addPostForm: FormGroup
+
+  ngOnInit() {
+    this.addPostForm = new FormGroup({
+      title: new FormControl(),
+      body: new FormControl(),
+      userId: new FormControl()
+    })
+  }
 
   // constructor() { }
 
-  onSubmit() {
-    this.submitted = true
+  addPost() {
+    const { title, body, userId } = this.addPostForm.controls
+    console.log('ADD POST VALUES!', title.value, body.value, userId.value)
   }
 }
